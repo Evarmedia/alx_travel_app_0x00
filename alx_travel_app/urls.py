73113bec618d796cdcdd369aeb732b36f78be491
Evarmedia <mishakmanuel@gmail.com>
2024-12-22 23:33:14 +0100

    Add models, serializers, and seed management command

--- alx_travel_app/urls.py
+++ alx_travel_app/urls.py
@@ -37,9 +37,10 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('maintravelapp.urls')),
-    path('listings/', include('listings.urls')),
-    path('', views.index, name='home'),  # Map the root URL to the index view
+    # path('api/', include('maintravelapp.urls')),
+    path('api/', include('listings.urls')),
+    # path('', views.index, name='home'),  # Map the root URL to the index view
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0),
          name='schema-swagger-ui'),
+    path('api-auth/', include('rest_framework.urls')),
 ]
--- test_env.py
+++
@@ -1,8 +0,0 @@
-import environ
-
-env = environ.Env()
-environ.Env.read_env()  # Reads the .env file
-
-# Print the secret key to check if it's loaded correctly
-print(env('DJANGO_SECRET_KEY'))
-
